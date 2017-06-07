03f5c48c5d1205b984e855ebd513a15dd466e04f
““geekersnow” <kaishiaini1314@126.com>
2017-06-07 14:41:13 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,6 +1,25 @@
 const loadAllItems = require('./loadAllItems.js');
 
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+    
+	var itemsList = loadAllItems();
+	var count = [],sum = 0; 
+	var textout= '***<没钱赚商店>购物清单***\n' ;
+	for(var i = 0;i < itemsList.length; i++ )
+		for(var j = 0; j < itemsList[i].length; j++)
+			if (inputs[i] == itemsList[j].barcode)
+			{
+				count[j] ++;
+			}
+		
+	for (var k=0; k<itemsList.length; k++)
+		if(count[k] != 0)
+		{
+			textout += '名称：'+itemsList[k].name+'，数量：'+ count[k]+itemsList[k].unit+'，单价：'+itemsList[k].price.toFixed(2)+'(元)，小计：'+(count[j] * itemsList[j].price).toFixed(2)+'(元)\n' 
+			sum += count[j] * itemsList[j].price;
+		}   
+	
+	textout += '----------------------\n总计：'+ sum.toFixed(2) +'(元)\n**********************';   	
+	console.log(textout);
 };
+
